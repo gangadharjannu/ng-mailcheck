fd69b000a84eeb3222ccb3900697e973c2fa530f
gangadharjannu <gangadhar.014@gmail.com>
2017-02-01 18:05:36 +0530

    Implemented mailcheck library in Angular way

--- mailcheck.directive.js
+++ mailcheck.directive.js
@@ -1,57 +1,52 @@
 
 (function () {
-    mailcheck.$inject = ['mailcheckService'];
-    function mailcheck(mailcheckService) {
+    mailcheck.$inject = ['$compile', '$timeout', 'mailcheckService'];
+    function mailcheck($compile, $timeout, mailcheckService) {
         var DDO = {
             restrict: 'EA',
+            require: '?ngModel',
             scope: {},
             bindToController: {
-
+                mailcheck: '='
             },
             controller: function () {
 
             },
-            controllerAs: 'mailcheck',
-            link: function (scope, elem, attrs) {
+            controllerAs: 'MC',
+            link: function (scope, element, attrs, ngModel) {
+                var result = null,
+                    templateStr = [
+                        '<span ng-show="completed">',
+                        '<span ng-show="suggestion">Did you mean <b ng-click="correctMe(suggestion.full)"><i ng-bind="suggestion.full"></i></b>?</span>',
+                        '<span ng-show="!suggestion">No Suggestions :(</span>',
+                        '</span>'
+                    ].join(''),
+                    defaultOptions = {
+                        templateStr: templateStr,
+                        replaceOptions: true
+                    },
+                    options = angular.extend({}, defaultOptions, scope.options);
 
-                var options = attrs['options'] || {},
-                    result = null,
-                    HTMLString = angular.element("<p class='email-hint'></p>"),
-                    value = null;
+                /* Implementation */
+                element.after($compile(options.templateStr)(scope))
+                element.on('blur', function (event) {
+                    options.email = ngModel.$viewValue;
+                    scope.completed = false;
+                    scope.suggestion = false;
+                    result = mailcheck.run(options);
+                    scope.$evalAsync(function () {
+                        scope.suggestion = result;
+                        scope.completed = true;
+                    });
 
-                HTMLString.on('click', function (event) {
-                    if (event.target.nodeName.match("I|B")) {
-                        scope.$evalAsync(function () {
-                            angular.element(elem).val(event.target.innerText)
-                            angular.element(elem.next())[0].style.visibility = "hidden";
-                        });
-                    }
                 });
-                angular.element(elem).on('blur', function (event) {
-                    value = event.target.value;
-                    if (value) {
-                        options.email = event.target.value;
-                        result = mailcheckService.run(options)
-                        if (result) {
-                            HTMLString = "Did you mean <b><i>" + result.full + "</i></b>?";
-                        } else {
-                            HTMLString = "No Suggestions :(";
-                        }
-                        angular.element(elem.next()).html(HTMLString);
-                        result && (angular.element(elem.next())[0].style.visibility = "visible");
-                    } else {
-                        return false;
-                    }
-                });
-                angular.element(elem).after(HTMLString);
-            },
-            template: [
-                // '<div>Hiii',
-                // 'How are you?',
-                // 'How do you do?</div>'
-            ].join('')
+                scope.correctMe = function (val) {
+                    ngModel.$setViewValue(val);
+                    ngModel.$render();
+                    scope.completed = false;
+                }
+            }
         };
-
         return DDO;
     }
     angular
