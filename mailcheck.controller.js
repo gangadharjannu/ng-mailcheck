040a83a7b8007da9e03e2e0d7341f656efee9e19
gangadharjannu <gangadhar.014@gmail.com>
2017-02-03 20:20:03 +0530

    Added options attribute to ng-mailcheck directive to pass user defined properties

--- mailcheck.controller.js
+++ mailcheck.controller.js
@@ -11,8 +11,15 @@
         //     onCompleteFunction: function () { },        // callback function for email correction
         //     replaceOptions: true                        // true=>replace options, false=>concat options
         // }
-        HC.mailcheck = {
-            'Hi':"hiii"
+        HC.mailcheckOptions = {
+            'Hi': "hiii",
+            "topLevelDomains": ['co.in'],
+            "replaceOptions": false
+        };
+        HC.mailcheckOptions1 = {
+            'Hi': "Byee",
+            "topLevelDomains": ['me'],
+            "replaceOptions": true
         };
     }
 
@@ -20,3 +27,32 @@
         .module('myApp')
         .controller('HomeController', HomeController);
 } ());
+
+
+
+// document.getElementById("transferForm").addEventListener("keypress", function (e) {
+//     e = e || event;
+//     var txtArea = /textarea/i.test((e.target || e.srcElement).tagName);
+//     return txtArea || (e.keyCode || e.which || e.charCode || 0) !== 13;
+// })
+
+// document.getElementById("quantity").addEventListener("keypress", function (event) {
+//     var keyCode = event.keyCode || event.which;
+//     if (keyCode === 13) {
+//         event.preventDefault();
+//         addItem();
+//     }
+// });
+// document.getElementById("quantity").addEventListener("keyup", function (event) {
+
+// });
+// document.getElementById("addButton").addEventListener("click", addItem);
+// function addItem() {
+//     // addItem code    
+// }
+
+// HC.mailcheck = {
+//     domains: mailcheckFactory.defaultDomains,
+//     secondLevelDomains: mailcheckFactory.defaultSecondLevelDomains,
+//     topLevelDomains:mailcheckFactory.defaultTopLevelDomains,
+// }
--- mailcheck.directive.js
+++ mailcheck.directive.js
@@ -1,58 +1,85 @@
 
-(function() {
+(function () {
     mailcheck.$inject = ['$compile', '$timeout', 'mailcheckFactory'];
     function mailcheck($compile, $timeout, mailcheckFactory) {
         var DDO = {
-            restrict: 'EA',
+            restrict: 'A',
             require: ['?ngModel', '?mailcheck'],
             scope: {},
             bindToController: {
-                mailcheck: '='
+                options: '='
             },
-            controller: function($scope, $element) {
+            controller: function ($scope, $element) {
                 console.log("Controller");
                 var MC = this;
                 MC.completed = false;
                 MC.suggestion = false;
             },
             controllerAs: 'MC',
-            link: function(scope, element, attrs, ctrls) {
+            link: function (scope, element, attrs, ctrls) {
                 console.log("Link");
                 // ngModel is an array [0] contains ngModel [1] contains directive's controller
                 var ngModelCtrl = ctrls[0],
                     MC = ctrls[1],
                     result = null,
                     templateStr = [
-                        '<span ng-show="MC.completed">',
+                        '<p ng-show="MC.completed">',
                         '<span ng-show="MC.suggestion">Did you mean <b ng-click="correctMe(MC.suggestion.full)"><i ng-bind="MC.suggestion.full"></i></b>?</span>',
                         '<span ng-show="!MC.suggestion">No Suggestions :(</span>',
-                        '</span>'
+                        '</p>'
                     ].join(''),
                     defaultOptions = {
                         templateStr: templateStr,
                         replaceOptions: true
                     },
-                    options = angular.extend({}, defaultOptions, scope.options);
+                    options = angular.extend({}, defaultOptions, MC.options),
+                    mailcheckDefaultOptions;
 
+                if (!options.replaceOptions) {
+                    // mailcheckDefaultOptions = angular.copy({
+                    //     domains: mailcheckFactory.defaultDomains,
+                    //     secondLevelDomains: mailcheckFactory.defaultSecondLevelDomains,
+                    //     topLevelDomains: mailcheckFactory.defaultTopLevelDomains
+                    // });
+                    mailcheckDefaultOptions = {
+                        domains: mailcheckFactory.defaultDomains,
+                        secondLevelDomains: mailcheckFactory.defaultSecondLevelDomains,
+                        topLevelDomains: mailcheckFactory.defaultTopLevelDomains
+                    };
+
+                    options = angular.extend({}, defaultOptions, concatOptions(MC.options, mailcheckDefaultOptions));
+                }
                 /* Implementation */
                 element.after($compile(options.templateStr)(scope))
-                element.on('blur', function(event) {
+                element.on('blur', function (event) {
                     options.email = ngModelCtrl.$viewValue;
                     MC.completed = false;
                     MC.suggestion = false;
                     result = mailcheckFactory.run(options);
-                    scope.$evalAsync(function() {
+                    scope.$evalAsync(function () {
                         MC.suggestion = result;
                         MC.completed = true;
                     });
 
                 });
-                scope.correctMe = function(val) {
+                scope.correctMe = function (val) {
                     ngModelCtrl.$setViewValue(val);
                     ngModelCtrl.$render();
-                    scope.$evalAsync(function() {
+                    scope.$evalAsync(function () {
                         MC.completed = false;
                     });
+                }
+                /* utility function for concatenating user specified options with mailcheck default options */
+                function concatOptions(dest, src) {
+                    var option;
+                    for (option in src) {
+                        if (src.hasOwnProperty(option)) {
+                            if (dest[option] && dest[option].length) {
+                                dest[option] = src[option].concat(dest[option]);
+                            }
+                        }
+                    }
+                    return dest;
                 }
             }
         };
